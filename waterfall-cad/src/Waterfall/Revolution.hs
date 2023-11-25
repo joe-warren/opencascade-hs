fc231b8546388f56d63653b4691523de2264fcbe
Joe Warren <hungryjoe.warren@gmail.com>
2023-11-25 12:15:32 +0000

    Make Revolution return solid

--- waterfall-cad/src/Waterfall/Revolution.hs
+++ waterfall-cad/src/Waterfall/Revolution.hs
@@ -2,18 +2,23 @@ module Waterfall.Revolution
 ( revolution
 ) where
 
-import Waterfall.Internal.Solid (Solid (..))
+import Waterfall.Internal.Solid (Solid (..), debug)
 import Waterfall.TwoD.Internal.Path (Path (..))
 import qualified OpenCascade.BRepPrimAPI.MakeRevol as MakeRevol
+import qualified OpenCascade.BRepBuilderAPI.MakeSolid as MakeSolid
 import qualified OpenCascade.BRepBuilderAPI.MakeShape as MakeShape
 import qualified OpenCascade.GP as GP
-import OpenCascade.Inheritance (upcast)
+import OpenCascade.Inheritance (upcast, unsafeDowncast)
 import Waterfall.Transforms (rotate)
+import Control.Monad.IO.Class (liftIO)
 import Linear (unit, _x)
 
 revolution :: Path -> Solid
-revolution (Path runPath) = rotate (unit _x) (pi/2) . Solid $ do
+revolution (Path runPath) = debug "revolution" .  rotate (unit _x) (pi/2) . Solid $ do
     p <- runPath
     axis <- GP.oy -- revolve around the y axis
     revol <- MakeRevol.fromShapeAndAx1 (upcast p) axis True
-    MakeShape.shape (upcast revol)
+    shell <- MakeShape.shape (upcast revol)
+    solidBuilder <- MakeSolid.new
+    liftIO $ MakeSolid.add solidBuilder =<< unsafeDowncast shell
+    MakeShape.shape (upcast solidBuilder)
