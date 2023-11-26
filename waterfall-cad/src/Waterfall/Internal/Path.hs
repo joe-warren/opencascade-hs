5eac2dfaa07568976d7547e870d15c18ea677aa7
Joseph Warren <hungryjoe.warren@gmail.com>
2023-11-26 18:19:14 +0000

    add signatures to Path typeclasses

--- waterfall-cad/src/Waterfall/Internal/Path.hs
+++ waterfall-cad/src/Waterfall/Internal/Path.hs
@@ -1,16 +1,17 @@
+{-# LANGUAGE  InstanceSigs#-}
 module Waterfall.Internal.Path
 ( Path (..)
 , joinPaths
 ) where
 
-import Data.Foldable (traverse_)
+import Data.List.NonEmpty (NonEmpty ())
+import Data.Foldable (traverse_, toList)
 import Data.Acquire
 import Control.Monad ((<=<))
 import Control.Monad.IO.Class (liftIO)
 import qualified OpenCascade.TopoDS as TopoDS
 import qualified OpenCascade.BRepBuilderAPI.MakeWire as MakeWire
 import Foreign.Ptr
-import Data.Foldable (toList)
 import Data.Semigroup (sconcat)
 
 newtype Path = Path { runPath :: Acquire (Ptr TopoDS.Wire) }
@@ -22,8 +23,13 @@ joinPaths paths = Path $ do
     MakeWire.wire builder
 
 instance Semigroup Path where
+    sconcat :: NonEmpty Path -> Path
     sconcat = joinPaths . toList
+    (<>) :: Path -> Path -> Path
+    a <> b = joinPaths [a, b]
     
 instance Monoid Path where
+    mempty :: Path
     mempty = joinPaths []
+    mconcat :: [Path] -> Path
     mconcat = joinPaths
