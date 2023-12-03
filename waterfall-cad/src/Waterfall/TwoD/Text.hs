def20966fa680ddea1bb1a52e777bc6c9854a161
Joseph Warren <hungryjoe.warren@gmail.com>
2023-12-03 18:45:40 +0000

    add documentation to text function

--- waterfall-cad/src/Waterfall/TwoD/Text.hs
+++ waterfall-cad/src/Waterfall/TwoD/Text.hs
@@ -9,6 +9,10 @@ import qualified OpenCascade.Font.BRepTextBuilder as BRepTextBuilder
 import qualified OpenCascade.Graphic3D.VerticalTextAlignment as VTA
 import qualified OpenCascade.Graphic3D.HorizontalTextAlignment as HTA
 
+-- | Render text, using the font from the provided filepath, at a given size.
+--
+-- The IO of actually loading the font/checking the file exists is defered 
+-- until the Shape is actually used
 text :: FilePath -> Double -> String -> Shape.Shape 
 text fontpath size content = Shape.Shape $ do
     axis <- GP.Ax3.new
