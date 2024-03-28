651c3005961f58dc648995b7bdbfb25708402bff
Joseph Warren <hungryjoe.warren@gmail.com>
2024-03-28 02:09:03 +0000

    Make Offset slightly more tolerant, this doesn't actually help with anything

--- waterfall-cad/src/Waterfall/Offset.hs
+++ waterfall-cad/src/Waterfall/Offset.hs
@@ -10,7 +10,27 @@ import qualified OpenCascade.BRepBuilderAPI.MakeShape as MakeShape
 import qualified OpenCascade.BRepOffset.Mode as Mode
 import qualified OpenCascade.GeomAbs.JoinType as GeomAbs.JoinType
 import qualified OpenCascade.BRepBuilderAPI.MakeSolid as MakeSolid
+import qualified OpenCascade.TopoDS.Types as TopoDS
+import qualified OpenCascade.TopExp.Explorer as TopExp.Explorer
+import qualified OpenCascade.TopAbs.ShapeEnum as TopAbs.ShapeEnum
+import Control.Monad (when)
+import Foreign.Ptr (Ptr)
+import Data.Acquire (Acquire)
 import Linear.Epsilon (nearZero)
+
+combineShellsToSolid :: Ptr TopoDS.Shape -> Acquire (Ptr TopoDS.Shape)
+combineShellsToSolid s = do
+    explorer <- TopExp.Explorer.new s TopAbs.ShapeEnum.Shell
+    makeSolid <- MakeSolid.new
+    let go = do
+            isMore <- liftIO $ TopExp.Explorer.more explorer
+            when isMore $ do
+                shell <- liftIO $ unsafeDowncast =<< TopExp.Explorer.value explorer
+                liftIO $ MakeSolid.add makeSolid shell
+                liftIO $ TopExp.Explorer.next explorer
+                go
+    go
+    upcast <$> MakeSolid.solid makeSolid
 
 -- | Expand or contract a `Solid` by a certain amount.
 -- 
@@ -29,7 +49,4 @@ offset value tolerance solid
     --liftIO $ MakeOffsetShape.performBySimple builder s value
     liftIO $ MakeOffsetShape.performByJoin builder s value tolerance Mode.Skin False False GeomAbs.JoinType.Arc False 
     shell <- MakeShape.shape (upcast builder)
-    shellBuilder <- MakeSolid.new
-    liftIO $ MakeSolid.add shellBuilder =<< unsafeDowncast shell
-    MakeShape.shape (upcast shellBuilder)
-    
+    combineShellsToSolid shell
