08055e766daf745e09f00a155d3f199cf8b860d3
Joseph Warren <hungryjoe.warren@gmail.com>
2024-03-30 00:09:47 +0000

    Make punctuation consistent

--- waterfall-cad/src/Waterfall.hs
+++ waterfall-cad/src/Waterfall.hs
@@ -10,10 +10,10 @@ module Waterfall
 -- The "Waterfall.Solids" module is the easiest way to construct 
 -- instances of `Solid`.
   module Waterfall.Solids
--- | The functions in this module can be used to transform `Solid`s
+-- | The functions in this module can be used to transform `Solid`s.
 , module Waterfall.Transforms
 -- | `Solid`'s can be combined together with 
--- [Constructive Solid Geometry](https://en.wikipedia.org/wiki/Constructive_solid_geometry)
+-- [Constructive Solid Geometry](https://en.wikipedia.org/wiki/Constructive_solid_geometry).
 , module Waterfall.Booleans
 , module Waterfall.Booleans.Operators
 -- | Once you've generated a `Solid`, 
@@ -21,32 +21,32 @@ module Waterfall
 -- 
 -- The `Waterfall.IO` module also supports reading `Solid`s from a variety of file formats.
 , module Waterfall.IO
--- | Calculating Axis Aligned Bounding Boxes from a `Solid`
+-- | Calculating Axis Aligned Bounding Boxes from a `Solid`.
 , module Waterfall.BoundingBox.AxisAligned
--- | Calculating Oriented Bounding Boxes from a `Solid`
+-- | Calculating Oriented Bounding Boxes from a `Solid`.
 , module Waterfall.BoundingBox.Oriented
 -- |  This module deals with adding rounds\/fillets\/bevels to a `Solid`.
 , module Waterfall.Fillet
--- |  This module deals with offsetting a `Solid`: either shrinking/expanding it
+-- |  This module deals with offsetting a `Solid`: either shrinking/expanding it.
 , module Waterfall.Offset
 -- | Paths in 3D space.
 , module Waterfall.Path
--- | Two Dimensional Shapes
+-- | Two Dimensional Shapes.
 , module Waterfall.TwoD.Shape
--- | Sweep a 2D `Shape` along a `Path`, constructing a `Solid`
+-- | Sweep a 2D `Shape` along a `Path`, constructing a `Solid`.
 , module Waterfall.Sweep
 -- | Construct a `Solid` of revolution from a `Path2D`.
 , module Waterfall.Revolution
--- | Transforms for data types that exist in Two Dimensional space, like `Shape` and `Path2D`
+-- | Transforms for data types that exist in Two Dimensional space, like `Shape` and `Path2D`.
 , module Waterfall.TwoD.Transforms
--- | Paths in 2D space, can be built up into a `Shape`
+-- | Paths in 2D space, can be built up into a `Shape`.
 , module Waterfall.TwoD.Path2D
--- | Create a `Shape` from text, rendered using a specific `Font` 
+-- | Create a `Shape` from text, rendered using a specific `Font`.
 , module Waterfall.TwoD.Text
 -- | Paths in 2D / 3D space.
 --
 -- This module defines functions that can be used with "Waterfall.Path" or "Waterfall.TwoD.Path2D".
--- Those modules both export monomorphized variants of the functions defined in this module
+-- Those modules both export monomorphized variants of the functions defined in this module.
 , module Waterfall.Path.Common
 )where
 
