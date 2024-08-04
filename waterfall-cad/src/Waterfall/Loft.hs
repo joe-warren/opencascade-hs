886e69ddb0c2a2dbadb078a24e1aaf69c6c1b9c8
Joseph Warren <hungryjoe.warren@gmail.com>
2024-08-04 20:20:50 +0100

    clean up whitespace

--- waterfall-cad/src/Waterfall/Loft.hs
+++ waterfall-cad/src/Waterfall/Loft.hs
@@ -6,7 +6,6 @@ Module: Waterfall.Loft
 Analagous to the [lofting](https://en.wikipedia.org/wiki/Lofting) process in boat building. 
 A loft is defined by planar cross-sections of the desired shape at chosen locations. 
 These cross-sections are then interpolated to form a smooth 3d shape.
-
 -}
 module Waterfall.Loft
 ( pointedLoft
@@ -25,11 +24,11 @@ import Control.Monad (forM_, (<=<))
 
 -- | Form a Loft which may terminate at defined points.
 --
--- If the start or end points are set to `Nothing` then one end of the loft will be the terminal cross section.
+-- If the start or end points are set to `Nothing` then one end of the loft will be the terminal cross-section.
 -- Otherwise, the loft will interpolate to that point.
 pointedLoft :: Double -- ^ The loft precision, this should be a small value, e.g. @ 1e-6 @ 
     -> Maybe (V3 Double) -- ^ Optional start point for the loft
-    -> [Path] -- ^ Series of cross sections that the loft will pass through
+    -> [Path] -- ^ Series of cross-sections that the loft will pass through
     -> Maybe (V3 Double) -- ^ Optional end point for the loft
     -> Solid
 pointedLoft precision start paths end = 
@@ -40,8 +39,8 @@ pointedLoft precision start paths end =
         forM_ end ((liftIO . ThruSections.addVertex thruSections) <=< v3ToVertex)
         MakeShape.shape (upcast thruSections)
 
--- | Form a loft between a series of cross sections.
+-- | Form a loft between a series of cross-sections.
 loft :: Double  -- ^ The loft precision, this should be a small value, e.g @ 1e-6 @
-    -> [Path] -- ^ Series of cross sections that the loft will pass through
+    -> [Path] -- ^ Series of cross-sections that the loft will pass through
     -> Solid
 loft precision paths = pointedLoft precision Nothing paths Nothing
