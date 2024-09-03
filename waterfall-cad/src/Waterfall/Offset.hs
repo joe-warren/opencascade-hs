aa1cc0ea53ba316f57f8e5b7f2a8bf66b02f3e84
Joe Warren <hungryjoe.warren@gmail.com>
2024-09-03 17:48:23 +0100

    Improve documentation around offset errors

--- waterfall-cad/src/Waterfall/Offset.hs
+++ waterfall-cad/src/Waterfall/Offset.hs
@@ -34,8 +34,18 @@ combineShellsToSolid s = do
 
 -- | Expand or contract a `Solid` by a certain amount.
 -- 
--- This seems to be relatively fragile on Sweeps/Prisms
--- (as in, may fail to produce a result)
+-- This is based on @MakeOffsetShape@ from the underlying OpenCascade library.
+-- And as such, only supports the same set of `Solid`s that @MakeOffsetShape@ does.
+--
+-- The documentation for @MakeOffsetShape@ lists the following constraints
+-- ( [link](https://dev.opencascade.org/doc/refman/html/class_b_rep_offset_a_p_i___make_offset_shape.html) ):
+--
+-- * All the faces of the shape S should be based on the surfaces with continuity at least C1.
+-- * The offset value should be sufficiently small to avoid self-intersections in resulting shape.
+--      Otherwise these self-intersections may appear inside an offset face if its initial surface is not plane or sphere or cylinder, also some non-adjacent offset faces may intersect each other. Also, some offset surfaces may "turn inside out".
+-- * The algorithm may fail if the shape S contains vertices where more than 3 edges converge.
+-- * Since 3d-offset algorithm involves intersection of surfaces, it is under limitations of surface intersection algorithm.
+-- * A result cannot be generated if the underlying geometry of S is BSpline with continuity C0.
 offset :: Double    -- ^ Amount to offset by, positive values expand, negative values contract
     -> Double       -- ^ Tolerance, this can be relatively small
     -> Solid        -- ^ the `Solid` to offset 
