dcc01629681070caec252ec6b9bf8357bd503c05
Joe Warren <hungryjoe.warren@gmail.com>
2025-01-30 23:21:17 +0000

    Update bw image to remove self intersecting curve, use xor in ReadFileExample

--- waterfall-cad-examples/src/SVG/ReadFileExample.hs
+++ waterfall-cad-examples/src/SVG/ReadFileExample.hs
@@ -5,8 +5,13 @@ module SVG.ReadFileExample
 import qualified Waterfall.SVG
 import qualified Waterfall.Solids as Solids
 import qualified Waterfall.TwoD.Shape as Shape
+import qualified Waterfall.Transforms as Transforms
+import qualified Waterfall.Booleans as Booleans
+import Linear (V3 (..))
 
 readFileExample :: FilePath -> IO Solids.Solid
 readFileExample filepath =
-    let solidify = mconcat . fmap (Solids.prism 1 . Shape.fromPath)
+    let expandVertically = Transforms.translate (V3 0 0 (-0.5)) . Transforms.scale (V3 1 1 2)
+        xor a b = (a `Booleans.difference` expandVertically b) <> (b `Booleans.difference` expandVertically a)
+        solidify = foldr xor Solids.nowhere . fmap (Solids.prism 1 . Shape.fromPath)
     in either (error . show) solidify <$> Waterfall.SVG.readSVG filepath
