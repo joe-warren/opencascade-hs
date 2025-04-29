f3e2010bf4e636e6e6f2fa08b6d668405b9be2d8
Joe Warren <hungryjoe.warren@gmail.com>
2025-04-29 10:50:22 +0100

    Rename Lenses module to Lens, and add additional lensy combinators

--- waterfall-cad/src/Waterfall.hs
+++ waterfall-cad/src/Waterfall.hs
@@ -53,7 +53,7 @@ module Waterfall
 -- | Generate 2D `Diagram`s from 3D `Shape`s
 , module Waterfall.Diagram
 -- | Lensy combinators
-, module Waterfall.Lenses
+, module Waterfall.Lens
 ) where
 
 import Waterfall.Booleans
@@ -75,4 +75,4 @@ import Waterfall.TwoD.Transforms
 import Waterfall.TwoD.Path2D
 import Waterfall.TwoD.Shape
 import Waterfall.TwoD.Text
-import Waterfall.Lenses
+import Waterfall.Lens
