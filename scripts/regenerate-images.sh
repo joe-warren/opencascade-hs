4f708f2cf4f3accfe574dd0256fd434833f3fe68
Joseph Warren <hungryjoe.warren@gmail.com>
2025-08-08 18:16:36 +0100

    replace font with Varela

--- scripts/regenerate-images.sh
+++ scripts/regenerate-images.sh
@@ -5,7 +5,7 @@ stack exec waterfall-cad-examples --  --gear --dark-mode-svg images/gear.svg
 stack exec waterfall-cad-examples --  --revolution --dark-mode-svg images/revolution.svg --height 200
 stack exec waterfall-cad-examples --  --sweep --dark-mode-svg images/sweep.svg --height 200
 stack exec waterfall-cad-examples --  --offset --dark-mode-svg images/offset.svg
-stack exec waterfall-cad-examples --  --text --content "Waterfall CAD" --dark-mode-svg images/text.svg --font images/fonts/nunito/Nunito-Black.ttf
+stack exec waterfall-cad-examples --  --text --content "Waterfall CAD" --dark-mode-svg images/text.svg --font images/fonts/varela/VarelaRound-Regular.ttf 
 stack exec waterfall-cad-examples --  --bound --dark-mode-svg images/bounding-boxes.svg --height 200
 stack exec waterfall-cad-examples --  --loft --dark-mode-svg images/loft.svg
 
@@ -14,7 +14,7 @@ stack exec waterfall-cad-examples --  --gear --glb images/models/gear.glb
 stack exec waterfall-cad-examples --  --revolution --glb images/models/revolution.glb
 stack exec waterfall-cad-examples --  --sweep --glb images/models/sweep.glb --resolution 0.01
 stack exec waterfall-cad-examples --  --offset --glb images/models/offset.glb
-stack exec waterfall-cad-examples --  --text --content "Waterfall CAD" --glb images/models/text.glb --font images/fonts/nunito/Nunito-Black.ttf --resolution 0.01
+stack exec waterfall-cad-examples --  --text --content "Waterfall CAD" --glb images/models/text.glb --font images/fonts/varela/VarelaRound-Regular.ttf --resolution 0.01
 stack exec waterfall-cad-examples --  --bound --glb images/models/bounding-boxes.glb
 stack exec waterfall-cad-examples --  --loft --glb images/models/loft.glb
 stack exec waterfall-cad-examples --  --fillet --glb images/models/fillet.glb
