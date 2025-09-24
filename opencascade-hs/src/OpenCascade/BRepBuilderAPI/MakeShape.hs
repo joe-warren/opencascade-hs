f0e8a3b5ec9cf11626f6b6b217d428793bc8220d
Joe Warren <hungryjoe.warren@gmail.com>
2025-09-24 14:53:51 +0100

    Migrate MakeShape to inline-c-cpp

--- opencascade-hs/src/OpenCascade/BRepBuilderAPI/MakeShape.hs
+++ opencascade-hs/src/OpenCascade/BRepBuilderAPI/MakeShape.hs
@@ -1,18 +1,32 @@
-{-# LANGUAGE CApiFFI #-}
 module OpenCascade.BRepBuilderAPI.MakeShape
 ( MakeShape
 , shape
 , build
 ) where
+
+import OpenCascade.BRepBuilderAPI.Internal.Context
+import OpenCascade.TopoDS.Internal.Context (topoDSContext)
 import OpenCascade.BRepBuilderAPI.Types
 import qualified OpenCascade.TopoDS as TopoDS
 import OpenCascade.TopoDS.Internal.Destructors (deleteShape)
 import Foreign.Ptr
-import Data.Acquire 
+import Data.Acquire
+import qualified Language.C.Inline.Cpp as C
+import qualified Language.C.Inline.Cpp.Exception as C
 
-foreign import capi unsafe "hs_BRepBuilderAPI_MakeShape.h hs_BRepBuilderAPI_MakeShape_shape" rawShape :: Ptr MakeShape -> IO (Ptr TopoDS.Shape)
+C.context (C.cppCtx <> topoDSContext <> brepBuilderAPIContext)
+
+C.include "<BRepBuilderAPI_MakeShape.hxx>"
+C.include "<TopoDS_Shape.hxx>"
 
 shape :: Ptr MakeShape -> Acquire (Ptr TopoDS.Shape)
-shape builder = mkAcquire (rawShape builder) (deleteShape)  
+shape builder =
+  let createShape = [C.throwBlock| TopoDS_Shape* {
+        return new TopoDS_Shape($(BRepBuilderAPI_MakeShape* builder)->Shape());
+      } |]
+  in mkAcquire createShape deleteShape
 
-foreign import capi unsafe "hs_BRepBuilderAPI_MakeShape.h hs_BRepBuilderAPI_MakeShape_build" build :: Ptr MakeShape -> IO ()
+build :: Ptr MakeShape -> IO ()
+build builder = [C.throwBlock| void {
+  $(BRepBuilderAPI_MakeShape* builder)->Build();
+} |]
