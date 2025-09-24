058716d458360102fca818b3a02bfe342ca59a69
Joe Warren <hungryjoe.warren@gmail.com>
2025-09-24 15:21:27 +0100

    Migrate BRep.Tool to inline-c-cpp

--- opencascade-hs/src/OpenCascade/BRep/Tool.hs
+++ opencascade-hs/src/OpenCascade/BRep/Tool.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CApiFFI #-}
 module OpenCascade.BRep.Tool
 ( curve
 , curveParamFirst
@@ -7,10 +6,15 @@ module OpenCascade.BRep.Tool
 , triangulation
 ) where
 
+import OpenCascade.TopoDS.Internal.Context (topoDSContext)
+import OpenCascade.TopLoc.Internal.Context (topLocContext)
+import OpenCascade.GP.Internal.Context (gpContext)
+import OpenCascade.Geom.Internal.Context (geomContext)
+import OpenCascade.Handle.Internal.Context (handleContext)
+import OpenCascade.Poly.Internal.Context (polyContext)
 import qualified OpenCascade.Geom as Geom
 import qualified OpenCascade.TopoDS as TopoDS
 import qualified OpenCascade.GP as GP
-
 import qualified OpenCascade.TopLoc.Types as TopLoc
 import qualified OpenCascade.Poly.Types as Poly
 import OpenCascade.Poly.Internal.Destructors (deleteHandleTriangulation)
@@ -19,30 +23,58 @@ import OpenCascade.Geom.Internal.Destructors (deleteHandleCurve)
 import OpenCascade.GP.Internal.Destructors (deletePnt)
 import Foreign.Ptr
 import Foreign.C
-import Data.Coerce
 import Data.Acquire (Acquire, mkAcquire)
+import qualified Language.C.Inline.Cpp as C
+import qualified Language.C.Inline.Cpp.Exception as C
 
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curve" rawCurve :: Ptr (TopoDS.Edge) -> IO(Ptr (Handle Geom.Curve))
+C.context (C.cppCtx <> topoDSContext <> topLocContext <> gpContext <> geomContext <> handleContext <> polyContext)
+
+C.include "<BRep_Tool.hxx>"
+C.include "<TopoDS_Edge.hxx>"
+C.include "<TopoDS_Vertex.hxx>"
+C.include "<TopoDS_Face.hxx>"
+C.include "<TopLoc_Location.hxx>"
+C.include "<Geom_Curve.hxx>"
+C.include "<Poly_Triangulation.hxx>"
+C.include "<Standard_Handle.hxx>"
+C.include "<gp_Pnt.hxx>"
 
 curve :: Ptr TopoDS.Edge -> Acquire (Ptr (Handle Geom.Curve))
-curve edge = mkAcquire (rawCurve edge) deleteHandleCurve
-
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curveParamFirst" rawCurveParamFirst :: Ptr (TopoDS.Edge) -> IO(CDouble)
+curve edge =
+  let createCurve = [C.throwBlock| opencascade::handle<Geom_Curve>* {
+        double first, last;
+        return new opencascade::handle<Geom_Curve>(BRep_Tool::Curve(*$(TopoDS_Edge* edge), first, last));
+      } |]
+  in mkAcquire createCurve deleteHandleCurve
 
 curveParamFirst :: Ptr TopoDS.Edge -> IO Double 
-curveParamFirst = coerce rawCurveParamFirst
-
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curveParamLast" rawCurveParamLast :: Ptr (TopoDS.Edge) -> IO(CDouble)
+curveParamFirst edge = do
+  result <- [C.throwBlock| double {
+    double first, last;
+    BRep_Tool::Curve(*$(TopoDS_Edge* edge), first, last);
+    return first;
+  } |]
+  return (realToFrac result)
 
 curveParamLast :: Ptr TopoDS.Edge -> IO Double 
-curveParamLast = coerce rawCurveParamLast
-
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_pnt" rawPnt :: Ptr (TopoDS.Vertex) -> IO (Ptr GP.Pnt)
+curveParamLast edge = do
+  result <- [C.throwBlock| double {
+    double first, last;
+    BRep_Tool::Curve(*$(TopoDS_Edge* edge), first, last);
+    return last;
+  } |]
+  return (realToFrac result)
 
 pnt :: Ptr TopoDS.Vertex -> Acquire (Ptr GP.Pnt)
-pnt v = mkAcquire (rawPnt v) deletePnt
-
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_triangulation" rawTriangulation :: Ptr (TopoDS.Face) -> Ptr TopLoc.Location -> IO(Ptr (Handle Poly.Triangulation))
+pnt vertex =
+  let createPnt = [C.throwBlock| gp_Pnt* {
+        return new gp_Pnt(BRep_Tool::Pnt(*$(TopoDS_Vertex* vertex)));
+      } |]
+  in mkAcquire createPnt deletePnt
 
 triangulation :: Ptr TopoDS.Face -> Ptr TopLoc.Location -> Acquire (Ptr (Handle Poly.Triangulation))
-triangulation face loc = mkAcquire (rawTriangulation face loc) deleteHandleTriangulation
+triangulation face loc =
+  let createTriangulation = [C.throwBlock| opencascade::handle<Poly_Triangulation>* {
+        return new opencascade::handle<Poly_Triangulation>(BRep_Tool::Triangulation(*$(TopoDS_Face* face), *$(TopLoc_Location* loc)));
+      } |]
+  in mkAcquire createTriangulation deleteHandleTriangulation
