74ec7326f70d5d3a8314bb7c56c2592ddf77d78d
Joe Warren <hungryjoe.warren@gmail.com>
2025-09-24 14:32:38 +0100

    Migrate NCollection to inline-c-cpp

--- opencascade-hs/src/OpenCascade/NCollection/Array1.hs
+++ opencascade-hs/src/OpenCascade/NCollection/Array1.hs
@@ -1,23 +1,37 @@
-{-# LANGUAGE CApiFFI #-}
 module OpenCascade.NCollection.Array1
 (
     newGPPntArray,
     setValueGPPnt
 ) where
 
+import OpenCascade.NCollection.Internal.Context
+import OpenCascade.GP.Internal.Context (gpContext)
 import Data.Acquire
 import Foreign.Ptr
 import Foreign.C
 import OpenCascade.GP.Types
 import OpenCascade.NCollection.Types
 import OpenCascade.NCollection.Internal.Destructors
+import qualified Language.C.Inline.Cpp as C
+import qualified Language.C.Inline.Cpp.Exception as C
 
-foreign import capi unsafe "hs_NCollection_Array1.h hs_new_NCollection_Array1_gp_Pnt" rawNewGPPntArray :: CInt -> CInt -> IO (Ptr (Array1 Pnt))
+C.context (C.cppCtx <> gpContext <> nCollectionContext)
+
+C.include "<NCollection_Array1.hxx>"
+C.include "<gp_Pnt.hxx>"
 
 newGPPntArray :: Int -> Int -> Acquire (Ptr (Array1 Pnt))
-newGPPntArray lo hi = mkAcquire (rawNewGPPntArray (fromIntegral lo) (fromIntegral hi)) deletePntArray
-
-foreign import capi unsafe "hs_NCollection_Array1.h hs_NCollection_Array1_gp_Pnt_setValue" rawSetValueGPPnt :: Ptr (Array1 Pnt) -> CInt -> Ptr Pnt -> IO ()
+newGPPntArray lo hi =
+  let cLo = fromIntegral lo
+      cHi = fromIntegral hi
+      createArray = [C.throwBlock| NCollection_Array1<gp_Pnt>* {
+        return new NCollection_Array1<gp_Pnt>($(int cLo), $(int cHi));
+      } |]
+  in mkAcquire createArray deletePntArray
 
 setValueGPPnt :: Ptr (Array1 Pnt) -> Int -> Ptr Pnt -> IO ()
-setValueGPPnt arr i p = rawSetValueGPPnt arr (fromIntegral i) p
+setValueGPPnt arr i p = do
+  let cIndex = fromIntegral i
+  [C.throwBlock| void {
+    $(NCollection_Array1<gp_Pnt>* arr)->SetValue($(int cIndex), *$(gp_Pnt* p));
+  } |]
--- opencascade-hs/src/OpenCascade/NCollection/Internal/Destructors.hs
+++ opencascade-hs/src/OpenCascade/NCollection/Internal/Destructors.hs
@@ -1,11 +1,22 @@
-{-# LANGUAGE CApiFFI #-}
 module OpenCascade.NCollection.Internal.Destructors
 ( deletePntArray
 ) where
 
+import OpenCascade.NCollection.Internal.Context
+import OpenCascade.GP.Internal.Context (gpContext)
 import OpenCascade.NCollection.Types
 import OpenCascade.GP.Types
 import Foreign.Ptr
+import qualified Language.C.Inline.Cpp as C
+import qualified Language.C.Inline.Cpp.Exception as C
 
-foreign import capi unsafe "hs_NCollection_Array1.h hs_delete_NCollection_Array1_gp_Pnt" deletePntArray :: Ptr (Array1 Pnt) -> IO ()
+C.context (C.cppCtx <> gpContext <> nCollectionContext)
+
+C.include "<NCollection_Array1.hxx>"
+C.include "<gp_Pnt.hxx>"
+
+deletePntArray :: Ptr (Array1 Pnt) -> IO ()
+deletePntArray arrayPtr = [C.throwBlock| void {
+  delete $(NCollection_Array1<gp_Pnt>* arrayPtr);
+} |]
 
