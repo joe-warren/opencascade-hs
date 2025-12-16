3b8e8b4f9a76ba36443875e63b62a456d1a6bebd
Joseph Warren <hungryjoe.warren@gmail.com>
2025-12-16 16:51:26 +0000

    Add generated Platonic Solids assets

--- waterfall-cad-examples/src/PlatonicSolidsExample.hs
+++ waterfall-cad-examples/src/PlatonicSolidsExample.hs
@@ -7,13 +7,13 @@ module PlatonicSolidsExample
 
 import qualified Waterfall.Transforms as Transforms
 import qualified Waterfall.Solids as Solids
-import Linear ((^*), unit, _x)
+import Linear ((^*), unit, _y)
 platonicSolidsExample :: Solids.Solid
 platonicSolidsExample = 
-    mconcat . zipWith (Transforms.translate . (unit _x ^*)) [0..] $
+    mconcat . zipWith (Transforms.translate . (unit _y ^*)) [0, -2.5 ..] $
         [ Solids.tetrahedron
         , Solids.centeredCube
         , Solids.octahedron
-        , Solids.dodecahedron
         , Solids.icosahedron
+        , Solids.dodecahedron
         ]
