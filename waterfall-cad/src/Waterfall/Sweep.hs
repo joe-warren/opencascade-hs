46446afe1d1ced68a411e4e037f39bde9dd05bdc
Joe Warren <hungryjoe.warren@gmail.com>
2026-04-15 16:58:06 +0100

    Migrate Sweep to use MigratePipeShell

--- waterfall-cad/src/Waterfall/Sweep.hs
+++ waterfall-cad/src/Waterfall/Sweep.hs
@@ -5,42 +5,30 @@ module Waterfall.Sweep
 import Waterfall.Internal.Solid (Solid (..), acquireSolid, solidFromAcquire)
 import Waterfall.Internal.Path (Path (..))
 import Waterfall.Internal.Path.Common (RawPath (..))
-import Waterfall.Internal.Edges (wireTangentStart, wireEndpoints)
+import Waterfall.Internal.Edges (allWires)
 import Waterfall.Internal.Finalizers (toAcquire)
-import Waterfall.Transforms (rotate, translate)
 import Waterfall.TwoD.Internal.Shape (Shape (..))
-import qualified OpenCascade.BRepOffsetAPI.MakePipe as MakePipe
+import qualified OpenCascade.BRepOffsetAPI.MakePipeShell as MakePipeShell
 import qualified OpenCascade.BRepBuilderAPI.MakeShape as MakeShape
 import OpenCascade.Inheritance (upcast)
-import qualified OpenCascade.TopoDS as TopoDS
 import Control.Monad.IO.Class (liftIO)
-import Foreign.Ptr
-import Linear (V3, normalize, unit, _x, _z, nearZero, cross, dot)
-import Data.Acquire (Acquire)
 import qualified Waterfall.Solids as Solids
-
-rotateFace :: V3 Double -> Ptr TopoDS.Shape -> Acquire (Ptr TopoDS.Shape)
-rotateFace v face = 
-    let vn = normalize v
-        z = unit _z
-        in if nearZero (vn - z)
-            then pure face
-            else
-                let axis = if nearZero (vn + z) then unit _x else z `cross` vn
-                    angle = acos (vn `dot` z)
-                in acquireSolid . rotate axis angle . solidFromAcquire . pure $ face 
-
-positionFace :: V3 Double -> Ptr TopoDS.Shape -> Acquire (Ptr TopoDS.Shape)
-positionFace p = acquireSolid . translate p . solidFromAcquire . pure
 
 -- | Sweep a 2D `Shape` along a `Path`, constructing a `Solid`
 sweep :: Path -> Shape -> Solid
 sweep (Path (ComplexRawPath theRawPath)) (Shape theRawShape) = solidFromAcquire $ do
     path <- toAcquire theRawPath
     shape <- toAcquire theRawShape
-    tangent <- liftIO $ wireTangentStart path
-    (start,_)  <- liftIO $ wireEndpoints path
-    adjustedFace <- positionFace start =<< rotateFace tangent shape
-    builder <- MakePipe.fromWireAndShape path adjustedFace
-    MakeShape.shape (upcast builder)
+    wires <- allWires shape
+    case wires of
+        (profileWire : _) -> do
+            builder <- MakePipeShell.new path
+            liftIO $ do
+                MakePipeShell.setModeFrenet builder True
+                MakePipeShell.add builder profileWire True True
+                MakeShape.build (upcast builder)
+                _ <- MakePipeShell.makeSolid builder
+                pure ()
+            MakeShape.shape (upcast builder)
+        [] -> acquireSolid Solids.emptySolid
 sweep _ _ = Solids.emptySolid
