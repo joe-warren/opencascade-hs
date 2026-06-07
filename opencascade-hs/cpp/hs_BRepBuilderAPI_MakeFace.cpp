a7089d2eb4c149863fbae3cd9089f0a9124884f2
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-07 23:18:08 +0100

    migrate MakeFace to new exception handling

--- opencascade-hs/cpp/hs_BRepBuilderAPI_MakeFace.cpp
+++ opencascade-hs/cpp/hs_BRepBuilderAPI_MakeFace.cpp
@@ -1,5 +1,6 @@
 #include <BRepBuilderAPI_MakeFace.hxx>
 #include <TopoDS_Face.hxx>
+#include "hs_Exception.h"
 #include "hs_BRepBuilderAPI_MakeFace.h"
 
 BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace(){
@@ -10,28 +11,82 @@ void hs_delete_BRepBuilderAPI_MakeFace(BRepBuilderAPI_MakeFace * builder){
     delete builder;
 }
 
-BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromFace(TopoDS_Face * face){
-    return new BRepBuilderAPI_MakeFace(*face);
+BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromFace(
+        TopoDS_Face * face,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [face]{
+        return new BRepBuilderAPI_MakeFace(*face);
+    });
 }
 
-BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromSurface(Handle(Geom_Surface)* surface, double tolerance){
-    return new BRepBuilderAPI_MakeFace(*surface, tolerance);
+BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromSurface(
+        Handle(Geom_Surface)* surface, double tolerance,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [surface, tolerance]{
+        return new BRepBuilderAPI_MakeFace(*surface, tolerance);
+    });
 }
 
-BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromSurfaceAndBounds(Handle(Geom_Surface)* surface, double uMin, double uMax, double vMin, double vMax, double tolerance){
-    return new BRepBuilderAPI_MakeFace(*surface, uMin, uMax, vMin, vMax, tolerance);
+BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromSurfaceAndBounds(
+        Handle(Geom_Surface)* surface, double uMin, double uMax, double vMin, double vMax, double tolerance,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [surface, uMin, uMax, vMin, vMax, tolerance]{
+        return new BRepBuilderAPI_MakeFace(*surface, uMin, uMax, vMin, vMax, tolerance);
+    });
 }
 
-BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromSurfaceAndWire(Handle(Geom_Surface)* surface, TopoDS_Wire* wire, bool inside){
-    return new BRepBuilderAPI_MakeFace(*surface, *wire, inside);
+BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromSurfaceAndWire(
+        Handle(Geom_Surface)* surface, TopoDS_Wire* wire, bool inside,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [surface, wire, inside]{
+        return new BRepBuilderAPI_MakeFace(*surface, *wire, inside);
+    });
 }
 
-BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromWire(TopoDS_Wire* wire, bool onlyPlane){
-    return new BRepBuilderAPI_MakeFace(*wire, onlyPlane);
+BRepBuilderAPI_MakeFace * hs_new_BRepBuilderAPI_MakeFace_fromWire(
+        TopoDS_Wire* wire, bool onlyPlane,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [wire, onlyPlane]{
+        return new BRepBuilderAPI_MakeFace(*wire, onlyPlane);
+    });
 }
 
-void hs_BRepBuilderAPI_MakeFace_Add(BRepBuilderAPI_MakeFace* builder, TopoDS_Wire* wire){
-    builder->Add(*wire);
+void hs_BRepBuilderAPI_MakeFace_Add(
+        BRepBuilderAPI_MakeFace* builder, TopoDS_Wire* wire,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    hs_handleExVoid(
+        exType,
+        exPtr,
+        [builder, wire]{
+        builder->Add(*wire);
+    });
 }
 
 bool hs_BRepBuilderAPI_MakeFace_IsDone(BRepBuilderAPI_MakeFace* builder){
@@ -42,6 +97,15 @@ BRepBuilderAPI_FaceError hs_BRepBuilderAPI_MakeFace_Error(BRepBuilderAPI_MakeFac
     return builder->Error();
 }
 
-TopoDS_Face * hs_BRepBuilderAPI_MakeFace_Face(BRepBuilderAPI_MakeFace * builder){
-    return new TopoDS_Face(builder->Face());
+TopoDS_Face * hs_BRepBuilderAPI_MakeFace_Face(
+        BRepBuilderAPI_MakeFace * builder,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [builder]{
+        return new TopoDS_Face(builder->Face());
+    });
 }
