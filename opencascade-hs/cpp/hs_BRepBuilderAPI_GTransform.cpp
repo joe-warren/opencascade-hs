5f7cfeb41a11669ef523fef218347d1ea90ad6c6
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-08 00:04:41 +0100

    Migrate GTransform onto new exception handling

--- opencascade-hs/cpp/hs_BRepBuilderAPI_GTransform.cpp
+++ opencascade-hs/cpp/hs_BRepBuilderAPI_GTransform.cpp
@@ -1,10 +1,20 @@
 #include <BRepBuilderAPI_GTransform.hxx>
 #include <TopoDS_Shape.hxx>
+#include "hs_Exception.h"
 #include "hs_BRepBuilderAPI_GTransform.h"
 
-TopoDS_Shape * hs_BRepBuilderAPI_GTransform_gtransform(TopoDS_Shape * shape, gp_GTrsf * trsf, bool copy){
-    auto builder = BRepBuilderAPI_GTransform(*shape, *trsf, copy);
-    return new TopoDS_Shape(builder.Shape());
+TopoDS_Shape * hs_BRepBuilderAPI_GTransform_gtransform(
+        TopoDS_Shape * shape, gp_GTrsf * trsf, bool copy,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [shape, trsf, copy]{
+        auto builder = BRepBuilderAPI_GTransform(*shape, *trsf, copy);
+        return new TopoDS_Shape(builder.Shape());
+    });
 }
 
 
