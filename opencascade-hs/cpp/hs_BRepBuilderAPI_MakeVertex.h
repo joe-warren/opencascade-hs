d6d463fa841d71eac6b26985bf5bf80d9be94af0
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-07 23:52:50 +0100

    Migrate MakeVertex to new exception handling

--- opencascade-hs/cpp/hs_BRepBuilderAPI_MakeVertex.h
+++ opencascade-hs/cpp/hs_BRepBuilderAPI_MakeVertex.h
@@ -7,11 +7,19 @@
 extern "C" {
 #endif
 
-BRepBuilderAPI_MakeVertex * hs_new_BRepBuilderAPI_MakeVertex_fromPnt(gp_Pnt* pnt);
+BRepBuilderAPI_MakeVertex * hs_new_BRepBuilderAPI_MakeVertex_fromPnt(
+    gp_Pnt* pnt,
+    HSExceptionType* exType,
+    void** exPtr
+);
 
 void hs_delete_BRepBuilderAPI_MakeVertex(BRepBuilderAPI_MakeVertex* builder);
 
-TopoDS_Vertex * hs_BRepBuilderAPI_MakeVertex_vertex(BRepBuilderAPI_MakeVertex * builder);
+TopoDS_Vertex * hs_BRepBuilderAPI_MakeVertex_vertex(
+    BRepBuilderAPI_MakeVertex * builder,
+    HSExceptionType* exType,
+    void** exPtr
+);
 
 #ifdef __cplusplus
 }
