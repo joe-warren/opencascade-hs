4656871966736e8697956a8834981b472cd5c1c5
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-08 23:28:21 +0100

    Migrate BRepMesh onto new exception handling

--- opencascade-hs/src/OpenCascade/BRepMesh/IncrementalMesh.hs
+++ opencascade-hs/src/OpenCascade/BRepMesh/IncrementalMesh.hs
@@ -7,14 +7,28 @@ module OpenCascade.BRepMesh.IncrementalMesh
 import OpenCascade.BRepMesh.Types (IncrementalMesh)
 import OpenCascade.BRepMesh.Internal.Destructors (deleteIncrementalMesh)
 import qualified OpenCascade.TopoDS as TopoDS
+import OpenCascade.Internal.Exception (wrapException)
+import Foreign.C (CInt)
 import Foreign.Ptr
 import Data.Acquire
 import Data.Coerce (coerce)
 
 
-foreign import capi unsafe "hs_BRepMesh_IncrementalMesh.h hs_BRepMesh_IncrementalMesh_fromShapeAndLinDeflection" rawFromShapeAndLinDeflection :: Ptr TopoDS.Shape -> Double ->  IO (Ptr IncrementalMesh)
+foreign import capi unsafe "hs_BRepMesh_IncrementalMesh.h hs_BRepMesh_IncrementalMesh_fromShapeAndLinDeflection" rawFromShapeAndLinDeflection
+    :: Ptr TopoDS.Shape
+    -> Double
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr IncrementalMesh)
 
 fromShapeAndLinDeflection :: Ptr TopoDS.Shape -> Double -> Acquire (Ptr IncrementalMesh)
-fromShapeAndLinDeflection shape linDeflection = mkAcquire (rawFromShapeAndLinDeflection shape (coerce linDeflection)) deleteIncrementalMesh
+fromShapeAndLinDeflection shape linDeflection = mkAcquire (wrapException $ rawFromShapeAndLinDeflection shape (coerce linDeflection)) deleteIncrementalMesh
 
-foreign import capi unsafe "hs_BRepMesh_IncrementalMesh.h hs_BRepMesh_IncrementalMesh_Perform" perform :: Ptr IncrementalMesh -> IO ()
+foreign import capi unsafe "hs_BRepMesh_IncrementalMesh.h hs_BRepMesh_IncrementalMesh_Perform" rawPerform
+    :: Ptr IncrementalMesh
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO ()
+
+perform :: Ptr IncrementalMesh -> IO ()
+perform mesh = wrapException $ rawPerform mesh
