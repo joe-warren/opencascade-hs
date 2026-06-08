4871d628016a1ab4acb2b3bbee1929c1320952a1
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-08 07:35:44 +0100

    Migrate MakePipe onto new exception handling

--- opencascade-hs/src/OpenCascade/BRepOffsetAPI/MakePipe.hs
+++ opencascade-hs/src/OpenCascade/BRepOffsetAPI/MakePipe.hs
@@ -9,17 +9,31 @@ import OpenCascade.BRepOffsetAPI.Types (MakePipe)
 import OpenCascade.BRepOffsetAPI.Internal.Destructors (deleteMakePipe)
 import qualified OpenCascade.TopoDS as TopoDS
 import qualified OpenCascade.GeomFill as GeomFill
+import OpenCascade.Internal.Exception (wrapException)
 import Foreign.Ptr
 import Foreign.C (CBool (..), CInt (..))
 import OpenCascade.Internal.Bool (boolToCBool)
 import Data.Acquire
 
-foreign import capi unsafe "hs_BRepOffsetAPI_MakePipe.h hs_new_BRepOffsetAPI_MakePipe_fromWireAndShape" rawFromWireAndShape :: Ptr TopoDS.Wire -> Ptr TopoDS.Shape -> IO (Ptr MakePipe)
+foreign import capi unsafe "hs_BRepOffsetAPI_MakePipe.h hs_new_BRepOffsetAPI_MakePipe_fromWireAndShape" rawFromWireAndShape
+    :: Ptr TopoDS.Wire
+    -> Ptr TopoDS.Shape
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr MakePipe)
 
 fromWireAndShape :: Ptr TopoDS.Wire -> Ptr TopoDS.Shape -> Acquire (Ptr MakePipe)
-fromWireAndShape wire profile = mkAcquire (rawFromWireAndShape wire profile) deleteMakePipe
+fromWireAndShape wire profile = mkAcquire (wrapException $ rawFromWireAndShape wire profile) deleteMakePipe
 
-foreign import capi unsafe "hs_BRepOffsetAPI_MakePipe.h hs_new_BRepOffsetAPI_MakePipe_fromWireShapeTrihedronModeAndForceC1" rawFromWireShapeTrihedronModeAndForceC1 :: Ptr TopoDS.Wire -> Ptr TopoDS.Shape -> CInt -> CBool -> IO (Ptr MakePipe)
+foreign import capi unsafe "hs_BRepOffsetAPI_MakePipe.h hs_new_BRepOffsetAPI_MakePipe_fromWireShapeTrihedronModeAndForceC1" rawFromWireShapeTrihedronModeAndForceC1
+    :: Ptr TopoDS.Wire
+    -> Ptr TopoDS.Shape
+    -> CInt
+    -> CBool
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr MakePipe)
+
 fromWireShapeTrihedronModeAndForceC1 :: Ptr TopoDS.Wire -> Ptr TopoDS.Shape -> GeomFill.Trihedron -> Bool -> Acquire (Ptr MakePipe)
 fromWireShapeTrihedronModeAndForceC1 wire profile trihedronMode forceC1 =
-     mkAcquire (rawFromWireShapeTrihedronModeAndForceC1 wire profile ((fromIntegral . fromEnum $ trihedronMode)) (boolToCBool forceC1)) deleteMakePipe
+     mkAcquire (wrapException $ rawFromWireShapeTrihedronModeAndForceC1 wire profile ((fromIntegral . fromEnum $ trihedronMode)) (boolToCBool forceC1)) deleteMakePipe
