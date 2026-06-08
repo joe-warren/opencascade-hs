f82f7f550166d42ee87d0a817f3ad54c5d4a16fe
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-08 08:05:53 +0100

    migrate MakeBox onto new exception handling

--- opencascade-hs/cpp/hs_BRepPrimAPI_MakeBox.cpp
+++ opencascade-hs/cpp/hs_BRepPrimAPI_MakeBox.cpp
@@ -1,21 +1,49 @@
 #include <BRepPrimAPI_MakeBox.hxx>
 #include <TopoDS_Solid.hxx>
 #include <TopoDS_Shell.hxx>
+#include "hs_Exception.h"
 #include "hs_BRepPrimAPI_MakeBox.h"
 
 
-BRepPrimAPI_MakeBox * hs_new_BRepPrimAPI_MakeBox_fromPnts(gp_Pnt *a, gp_Pnt *b){
-    return new BRepPrimAPI_MakeBox(*a, *b);
+BRepPrimAPI_MakeBox * hs_new_BRepPrimAPI_MakeBox_fromPnts(
+        gp_Pnt *a, gp_Pnt *b,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [a, b]{
+        return new BRepPrimAPI_MakeBox(*a, *b);
+    });
 }
 
 void hs_delete_BRepPrimAPI_MakeBox(BRepPrimAPI_MakeBox* builder){
     delete builder;
 }
 
-TopoDS_Shape * hs_BRepPrimAPI_MakeBox_Solid(BRepPrimAPI_MakeBox* builder){
-    return new TopoDS_Shape(builder->Solid());
+TopoDS_Shape * hs_BRepPrimAPI_MakeBox_Solid(
+        BRepPrimAPI_MakeBox* builder,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [builder]{
+        return new TopoDS_Shape(builder->Solid());
+    });
 }
 
-TopoDS_Shell * hs_BRepPrimAPI_MakeBox_Shell(BRepPrimAPI_MakeBox* builder){
-    return new TopoDS_Shell(builder->Shell());
+TopoDS_Shell * hs_BRepPrimAPI_MakeBox_Shell(
+        BRepPrimAPI_MakeBox* builder,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [builder]{
+        return new TopoDS_Shell(builder->Shell());
+    });
 }
