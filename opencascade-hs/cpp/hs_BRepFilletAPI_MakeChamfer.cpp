c85dcf6f2e5d989d3a3cabc7034d79039fe624e2
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-08 07:32:49 +0100

    Migrate MakeChamfer to new exception handling

--- opencascade-hs/cpp/hs_BRepFilletAPI_MakeChamfer.cpp
+++ opencascade-hs/cpp/hs_BRepFilletAPI_MakeChamfer.cpp
@@ -1,21 +1,49 @@
 #include <BRepFilletAPI_MakeChamfer.hxx>
 #include <TopoDS_Shape.hxx>
+#include "hs_Exception.h"
 #include "hs_BRepFilletAPI_MakeChamfer.h"
 
-BRepFilletAPI_MakeChamfer * hs_new_BRepFilletAPI_MakeChamfer_fromShape(TopoDS_Shape * shape) {
-    return new BRepFilletAPI_MakeChamfer(*shape);
+BRepFilletAPI_MakeChamfer * hs_new_BRepFilletAPI_MakeChamfer_fromShape(
+        TopoDS_Shape * shape,
+        HSExceptionType* exType,
+        void** exPtr
+) {
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [shape]{
+        return new BRepFilletAPI_MakeChamfer(*shape);
+    });
 }
 
 void hs_delete_BRepFilletAPI_MakeChamfer(BRepFilletAPI_MakeChamfer * builder) {
     delete builder;
 }
 
-void hs_BRepFilletAPI_MakeChamfer_addEdge(BRepFilletAPI_MakeChamfer * builder, TopoDS_Edge *edge ){
-    builder->Add(*edge);
+void hs_BRepFilletAPI_MakeChamfer_addEdge(
+        BRepFilletAPI_MakeChamfer * builder, TopoDS_Edge *edge,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    hs_handleExVoid(
+        exType,
+        exPtr,
+        [builder, edge]{
+        builder->Add(*edge);
+    });
 }
 
-void hs_BRepFilletAPI_MakeChamfer_addEdgeWithDistance(BRepFilletAPI_MakeChamfer * builder, double d, TopoDS_Edge *edge ){
-    builder->Add(d, *edge);
+void hs_BRepFilletAPI_MakeChamfer_addEdgeWithDistance(
+        BRepFilletAPI_MakeChamfer * builder, double d, TopoDS_Edge *edge,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    hs_handleExVoid(
+        exType,
+        exPtr,
+        [builder, d, edge]{
+        builder->Add(d, *edge);
+    });
 }
 
 void hs_BRepFilletAPI_MakeChamfer_reset(BRepFilletAPI_MakeChamfer * builder){
@@ -26,10 +54,28 @@ int hs_BRepFilletAPI_MakeChamfer_nbEdges(BRepFilletAPI_MakeChamfer * builder, in
     return builder->NbEdges(contourIndex);
 }
 
-TopoDS_Edge * hs_BRepFilletAPI_MakeChamfer_edge(BRepFilletAPI_MakeChamfer * builder, int contourIndex, int edgeIndex){
-    return new TopoDS_Edge(builder->Edge(contourIndex, edgeIndex));
+TopoDS_Edge * hs_BRepFilletAPI_MakeChamfer_edge(
+        BRepFilletAPI_MakeChamfer * builder, int contourIndex, int edgeIndex,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [builder, contourIndex, edgeIndex]{
+        return new TopoDS_Edge(builder->Edge(contourIndex, edgeIndex));
+    });
 }
 
-void hs_BRepFilletAPI_MakeChamfer_remove(BRepFilletAPI_MakeChamfer * builder, TopoDS_Edge * edge){
-    builder->Remove(*edge);
+void hs_BRepFilletAPI_MakeChamfer_remove(
+        BRepFilletAPI_MakeChamfer * builder, TopoDS_Edge * edge,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    hs_handleExVoid(
+        exType,
+        exPtr,
+        [builder, edge]{
+        builder->Remove(*edge);
+    });
 }
--- opencascade-hs/cpp/hs_BRepFilletAPI_MakeChamfer.h
+++ opencascade-hs/cpp/hs_BRepFilletAPI_MakeChamfer.h
@@ -7,21 +7,41 @@
 extern "C" {
 #endif
 
-BRepFilletAPI_MakeChamfer * hs_new_BRepFilletAPI_MakeChamfer_fromShape(TopoDS_Shape * shape) ;
+BRepFilletAPI_MakeChamfer * hs_new_BRepFilletAPI_MakeChamfer_fromShape(
+    TopoDS_Shape * shape,
+    HSExceptionType* exType,
+    void** exPtr
+);
 
 void hs_delete_BRepFilletAPI_MakeChamfer(BRepFilletAPI_MakeChamfer * builder) ;
 
-void hs_BRepFilletAPI_MakeChamfer_addEdge(BRepFilletAPI_MakeChamfer * builder, TopoDS_Edge *edge );
+void hs_BRepFilletAPI_MakeChamfer_addEdge(
+    BRepFilletAPI_MakeChamfer * builder, TopoDS_Edge *edge,
+    HSExceptionType* exType,
+    void** exPtr
+);
 
-void hs_BRepFilletAPI_MakeChamfer_addEdgeWithDistance(BRepFilletAPI_MakeChamfer * builder, double d, TopoDS_Edge *edge );
+void hs_BRepFilletAPI_MakeChamfer_addEdgeWithDistance(
+    BRepFilletAPI_MakeChamfer * builder, double d, TopoDS_Edge *edge,
+    HSExceptionType* exType,
+    void** exPtr
+);
 
 void hs_BRepFilletAPI_MakeChamfer_reset(BRepFilletAPI_MakeChamfer * builder);
 
 int hs_BRepFilletAPI_MakeChamfer_nbEdges(BRepFilletAPI_MakeChamfer * builder, int contourIndex);
 
-TopoDS_Edge * hs_BRepFilletAPI_MakeChamfer_edge(BRepFilletAPI_MakeChamfer * builder, int contourIndex, int edgeIndex);
+TopoDS_Edge * hs_BRepFilletAPI_MakeChamfer_edge(
+    BRepFilletAPI_MakeChamfer * builder, int contourIndex, int edgeIndex,
+    HSExceptionType* exType,
+    void** exPtr
+);
 
-void hs_BRepFilletAPI_MakeChamfer_remove(BRepFilletAPI_MakeChamfer * builder, TopoDS_Edge * edge);
+void hs_BRepFilletAPI_MakeChamfer_remove(
+    BRepFilletAPI_MakeChamfer * builder, TopoDS_Edge * edge,
+    HSExceptionType* exType,
+    void** exPtr
+);
 
 #ifdef __cplusplus
 }
