0af1f692079609886192660d99e847491809d1fa
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-08 08:17:53 +0100

    Migrate MakeRevol onto new Exception handling

--- opencascade-hs/cpp/hs_BRepPrimAPI_MakeRevol.cpp
+++ opencascade-hs/cpp/hs_BRepPrimAPI_MakeRevol.cpp
@@ -1,8 +1,18 @@
 #include <BRepPrimAPI_MakeRevol.hxx>
+#include "hs_Exception.h"
 #include "hs_BRepPrimAPI_MakeRevol.h"
 
-BRepPrimAPI_MakeRevol * hs_new_BRepPrimAPI_MakeRevol_fromShapeAndAx1(TopoDS_Shape * shape, gp_Ax1 * axis, bool copy){
-    return new BRepPrimAPI_MakeRevol(*shape, *axis, copy);
+BRepPrimAPI_MakeRevol * hs_new_BRepPrimAPI_MakeRevol_fromShapeAndAx1(
+        TopoDS_Shape * shape, gp_Ax1 * axis, bool copy,
+        HSExceptionType* exType,
+        void** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [shape, axis, copy]{
+        return new BRepPrimAPI_MakeRevol(*shape, *axis, copy);
+    });
 }
 
 void hs_delete_BRepPrimAPI_MakeRevol(BRepPrimAPI_MakeRevol * builder){
--- opencascade-hs/cpp/hs_BRepPrimAPI_MakeRevol.h
+++ opencascade-hs/cpp/hs_BRepPrimAPI_MakeRevol.h
@@ -7,7 +7,11 @@
 extern "C" {
 #endif
 
-BRepPrimAPI_MakeRevol * hs_new_BRepPrimAPI_MakeRevol_fromShapeAndAx1(TopoDS_Shape * shape, gp_Ax1 * axis, bool copy);
+BRepPrimAPI_MakeRevol * hs_new_BRepPrimAPI_MakeRevol_fromShapeAndAx1(
+    TopoDS_Shape * shape, gp_Ax1 * axis, bool copy,
+    HSExceptionType* exType,
+    void** exPtr
+);
 
 void hs_delete_BRepPrimAPI_MakeRevol(BRepPrimAPI_MakeRevol * builder);
 
