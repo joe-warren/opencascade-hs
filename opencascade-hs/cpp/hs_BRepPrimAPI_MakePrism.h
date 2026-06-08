141ae65eacf48067dcb91fb3049115abfc655a82
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-08 08:23:24 +0100

    Migrate MakePrism onto new exception handling

--- opencascade-hs/cpp/hs_BRepPrimAPI_MakePrism.h
+++ opencascade-hs/cpp/hs_BRepPrimAPI_MakePrism.h
@@ -7,9 +7,15 @@
 extern "C" {
 #endif
 
-TopoDS_Shape * hs_BRepPrimAPI_MakePrism_fromVec(TopoDS_Shape * shape, gp_Vec * vec, bool copy, bool canonize);
+TopoDS_Shape * hs_BRepPrimAPI_MakePrism_fromVec(TopoDS_Shape * shape, gp_Vec * vec, bool copy, bool canonize, 
+        HSExceptionType* exType,
+        void** exPtr
+);
 
-TopoDS_Shape * hs_BRepPrimAPI_MakePrism_fromDir(TopoDS_Shape * shape, gp_Dir * dir, bool inf, bool copy, bool canonize);
+TopoDS_Shape * hs_BRepPrimAPI_MakePrism_fromDir(TopoDS_Shape * shape, gp_Dir * dir, bool inf, bool copy, bool canonize, 
+        HSExceptionType* exType,
+        void** exPtr
+);
 
 #ifdef __cplusplus
 }
