b8dccb1e9078fc57e2366fa3d5493f7d57568471
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-09 16:14:26 +0100

    Migrate TopoDS_Builder onto new exception handling

--- opencascade-hs/cpp/hs_TopoDS_Builder.cpp
+++ opencascade-hs/cpp/hs_TopoDS_Builder.cpp
@@ -1,4 +1,5 @@
 #include <TopoDS_Builder.hxx>
+#include "hs_Exception.h"
 #include "hs_TopoDS_Builder.h"
 
 TopoDS_Builder * hs_new_TopoDS_Builder(void){
@@ -10,33 +11,68 @@ void hs_delete_TopoDS_Builder(TopoDS_Builder * builder){
 }
 
 
-void hs_TopoDS_Builder_makeWire(TopoDS_Builder * builder, TopoDS_Wire * wire){
-    builder->MakeWire(*wire);
+void hs_TopoDS_Builder_makeWire(
+        TopoDS_Builder * builder, TopoDS_Wire * wire,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(exType, exPtr, [builder, wire]{
+        builder->MakeWire(*wire);
+    });
 }
 
 
-void hs_TopoDS_Builder_makeShell(TopoDS_Builder * builder, TopoDS_Shell * shell){
-    builder->MakeShell(*shell);
+void hs_TopoDS_Builder_makeShell(
+        TopoDS_Builder * builder, TopoDS_Shell * shell,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(exType, exPtr, [builder, shell]{
+        builder->MakeShell(*shell);
+    });
 }
 
-void hs_TopoDS_Builder_makeSolid(TopoDS_Builder * builder, TopoDS_Solid * solid){
-    builder->MakeSolid(*solid);
+void hs_TopoDS_Builder_makeSolid(
+        TopoDS_Builder * builder, TopoDS_Solid * solid,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(exType, exPtr, [builder, solid]{
+        builder->MakeSolid(*solid);
+    });
 }
 
-void hs_TopoDS_Builder_makeCompSolid(TopoDS_Builder * builder, TopoDS_CompSolid * solid){
-    builder->MakeCompSolid(*solid);
+void hs_TopoDS_Builder_makeCompSolid(
+        TopoDS_Builder * builder, TopoDS_CompSolid * solid,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(exType, exPtr, [builder, solid]{
+        builder->MakeCompSolid(*solid);
+    });
 }
 
-void hs_TopoDS_Builder_makeCompound(TopoDS_Builder * builder, TopoDS_Compound * compound){
-    builder->MakeCompound(*compound);
+void hs_TopoDS_Builder_makeCompound(
+        TopoDS_Builder * builder, TopoDS_Compound * compound,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(exType, exPtr, [builder, compound]{
+        builder->MakeCompound(*compound);
+    });
 }
 
-void hs_TopoDS_Builder_add(TopoDS_Builder * builder, TopoDS_Shape * s, TopoDS_Shape * c){
-    builder->Add(*s, *c);
+void hs_TopoDS_Builder_add(
+        TopoDS_Builder * builder, TopoDS_Shape * s, TopoDS_Shape * c,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(exType, exPtr, [builder, s, c]{
+        builder->Add(*s, *c);
+    });
 }
 
-void hs_TopoDS_Builder_remove(TopoDS_Builder * builder, TopoDS_Shape * s, TopoDS_Shape * c){
-    builder->Remove(*s, *c);
+void hs_TopoDS_Builder_remove(
+        TopoDS_Builder * builder, TopoDS_Shape * s, TopoDS_Shape * c,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(exType, exPtr, [builder, s, c]{
+        builder->Remove(*s, *c);
+    });
 }
 
 
--- opencascade-hs/src/OpenCascade/TopoDS/Builder.hs
+++ opencascade-hs/src/OpenCascade/TopoDS/Builder.hs
@@ -11,8 +11,10 @@ module OpenCascade.TopoDS.Builder
 , remove
 ) where
 
-import OpenCascade.TopoDS.Types 
+import OpenCascade.TopoDS.Types
 import OpenCascade.TopoDS.Internal.Destructors (deleteBuilder)
+import OpenCascade.Internal.Exception (wrapException)
+import Foreign.C (CInt)
 import Foreign.Ptr (Ptr)
 import Data.Acquire (Acquire, mkAcquire)
 
@@ -21,16 +23,44 @@ foreign import capi unsafe "hs_TopoDS_Builder.h hs_new_TopoDS_Builder" rawNew ::
 new :: Acquire (Ptr Builder)
 new = mkAcquire rawNew deleteBuilder
 
-foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeWire" makeWire :: Ptr Builder -> Ptr Wire -> IO ()
+foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeWire" rawMakeWire
+    :: Ptr Builder -> Ptr Wire -> Ptr CInt -> Ptr (Ptr ()) -> IO ()
 
-foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeShell" makeShell :: Ptr Builder -> Ptr Shell -> IO ()
+makeWire :: Ptr Builder -> Ptr Wire -> IO ()
+makeWire builder wire = wrapException $ rawMakeWire builder wire
 
-foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeSolid" makeSolid :: Ptr Builder -> Ptr Solid -> IO ()
+foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeShell" rawMakeShell
+    :: Ptr Builder -> Ptr Shell -> Ptr CInt -> Ptr (Ptr ()) -> IO ()
 
-foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeCompSolid" makeCompSolid :: Ptr Builder -> Ptr CompSolid -> IO ()
+makeShell :: Ptr Builder -> Ptr Shell -> IO ()
+makeShell builder shell = wrapException $ rawMakeShell builder shell
 
-foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeCompound" makeCompound :: Ptr Builder -> Ptr Compound -> IO ()
+foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeSolid" rawMakeSolid
+    :: Ptr Builder -> Ptr Solid -> Ptr CInt -> Ptr (Ptr ()) -> IO ()
 
-foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_add" add :: Ptr Builder -> Ptr Shape -> Ptr Shape -> IO ()
+makeSolid :: Ptr Builder -> Ptr Solid -> IO ()
+makeSolid builder solid = wrapException $ rawMakeSolid builder solid
 
-foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_remove" remove :: Ptr Builder -> Ptr Shape -> Ptr Shape -> IO ()
+foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeCompSolid" rawMakeCompSolid
+    :: Ptr Builder -> Ptr CompSolid -> Ptr CInt -> Ptr (Ptr ()) -> IO ()
+
+makeCompSolid :: Ptr Builder -> Ptr CompSolid -> IO ()
+makeCompSolid builder solid = wrapException $ rawMakeCompSolid builder solid
+
+foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_makeCompound" rawMakeCompound
+    :: Ptr Builder -> Ptr Compound -> Ptr CInt -> Ptr (Ptr ()) -> IO ()
+
+makeCompound :: Ptr Builder -> Ptr Compound -> IO ()
+makeCompound builder compound = wrapException $ rawMakeCompound builder compound
+
+foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_add" rawAdd
+    :: Ptr Builder -> Ptr Shape -> Ptr Shape -> Ptr CInt -> Ptr (Ptr ()) -> IO ()
+
+add :: Ptr Builder -> Ptr Shape -> Ptr Shape -> IO ()
+add builder s c = wrapException $ rawAdd builder s c
+
+foreign import capi unsafe "hs_TopoDS_Builder.h hs_TopoDS_Builder_remove" rawRemove
+    :: Ptr Builder -> Ptr Shape -> Ptr Shape -> Ptr CInt -> Ptr (Ptr ()) -> IO ()
+
+remove :: Ptr Builder -> Ptr Shape -> Ptr Shape -> IO ()
+remove builder s c = wrapException $ rawRemove builder s c
