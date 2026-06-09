ba33503408a71cbc378c2b3e6ef8c6dea7dc795d
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-09 08:10:37 +0100

    migrate BRep_Tool onto new exception handling

--- opencascade-hs/cpp/hs_BRep_Tool.cpp
+++ opencascade-hs/cpp/hs_BRep_Tool.cpp
@@ -1,30 +1,78 @@
 #include <BRep_Tool.hxx>
 #include <Standard_Handle.hxx>
+#include "hs_Exception.h"
 #include "hs_BRep_Tool.h"
 
-Handle(Geom_Curve) * hs_BRep_Tool_curve(TopoDS_Edge * edge){
-    double s, e;
-    return new Handle(Geom_Curve)(BRep_Tool::Curve(*edge, s, e));
+Handle(Geom_Curve) * hs_BRep_Tool_curve(
+        TopoDS_Edge * edge,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [edge]{
+            double s, e;
+            return new Handle(Geom_Curve)(BRep_Tool::Curve(*edge, s, e));
+        }
+    );
 }
 
-double hs_BRep_Tool_curveParamFirst(TopoDS_Edge * edge){
-    double s, e;
-    BRep_Tool::Curve(*edge, s, e);
-    return s;
+double hs_BRep_Tool_curveParamFirst(
+        TopoDS_Edge * edge,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleExWithDefault(
+        exType,
+        exPtr,
+        [edge]{
+            double s, e;
+            BRep_Tool::Curve(*edge, s, e);
+            return s;
+        },
+        0.0
+    );
 }
 
-double hs_BRep_Tool_curveParamLast(TopoDS_Edge * edge){
-    double s, e;
-    BRep_Tool::Curve(*edge, s, e);
-    return e;
+double hs_BRep_Tool_curveParamLast(
+        TopoDS_Edge * edge,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleExWithDefault(
+        exType,
+        exPtr,
+        [edge]{
+            double s, e;
+            BRep_Tool::Curve(*edge, s, e);
+            return e;
+        },
+        0.0
+    );
 }
 
-gp_Pnt * hs_BRep_Tool_pnt(TopoDS_Vertex * vertex){
-    return new gp_Pnt(BRep_Tool::Pnt(*vertex));
-} 
+gp_Pnt * hs_BRep_Tool_pnt(
+        TopoDS_Vertex * vertex,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [vertex]{
+            return new gp_Pnt(BRep_Tool::Pnt(*vertex));
+        }
+    );
+}
 
-Handle(Poly_Triangulation) * hs_BRep_Tool_triangulation(TopoDS_Face * face, TopLoc_Location * loc){
-    return new opencascade::handle<Poly_Triangulation>(BRep_Tool::Triangulation(*face, *loc));
+Handle(Poly_Triangulation) * hs_BRep_Tool_triangulation(
+        TopoDS_Face * face, TopLoc_Location * loc,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [face, loc]{
+            return new opencascade::handle<Poly_Triangulation>(BRep_Tool::Triangulation(*face, *loc));
+        }
+    );
 }
 
 
--- opencascade-hs/src/OpenCascade/BRep/Tool.hs
+++ opencascade-hs/src/OpenCascade/BRep/Tool.hs
@@ -17,32 +17,54 @@ import OpenCascade.Poly.Internal.Destructors (deleteHandleTriangulation)
 import OpenCascade.Handle (Handle)
 import OpenCascade.Geom.Internal.Destructors (deleteHandleCurve)
 import OpenCascade.GP.Internal.Destructors (deletePnt)
+import OpenCascade.Internal.Exception (wrapException)
 import Foreign.Ptr
 import Foreign.C
 import Data.Coerce
 import Data.Acquire (Acquire, mkAcquire)
 
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curve" rawCurve :: Ptr (TopoDS.Edge) -> IO(Ptr (Handle Geom.Curve))
+foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curve" rawCurve
+    :: Ptr (TopoDS.Edge)
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr (Handle Geom.Curve))
 
 curve :: Ptr TopoDS.Edge -> Acquire (Ptr (Handle Geom.Curve))
-curve edge = mkAcquire (rawCurve edge) deleteHandleCurve
+curve edge = mkAcquire (wrapException $ rawCurve edge) deleteHandleCurve
 
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curveParamFirst" rawCurveParamFirst :: Ptr (TopoDS.Edge) -> IO(CDouble)
+foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curveParamFirst" rawCurveParamFirst
+    :: Ptr (TopoDS.Edge)
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (CDouble)
 
-curveParamFirst :: Ptr TopoDS.Edge -> IO Double 
-curveParamFirst = coerce rawCurveParamFirst
+curveParamFirst :: Ptr TopoDS.Edge -> IO Double
+curveParamFirst edge = coerce <$> wrapException (rawCurveParamFirst edge)
 
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curveParamLast" rawCurveParamLast :: Ptr (TopoDS.Edge) -> IO(CDouble)
+foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_curveParamLast" rawCurveParamLast
+    :: Ptr (TopoDS.Edge)
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (CDouble)
 
-curveParamLast :: Ptr TopoDS.Edge -> IO Double 
-curveParamLast = coerce rawCurveParamLast
+curveParamLast :: Ptr TopoDS.Edge -> IO Double
+curveParamLast edge = coerce <$> wrapException (rawCurveParamLast edge)
 
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_pnt" rawPnt :: Ptr (TopoDS.Vertex) -> IO (Ptr GP.Pnt)
+foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_pnt" rawPnt
+    :: Ptr (TopoDS.Vertex)
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr GP.Pnt)
 
 pnt :: Ptr TopoDS.Vertex -> Acquire (Ptr GP.Pnt)
-pnt v = mkAcquire (rawPnt v) deletePnt
+pnt v = mkAcquire (wrapException $ rawPnt v) deletePnt
 
-foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_triangulation" rawTriangulation :: Ptr (TopoDS.Face) -> Ptr TopLoc.Location -> IO(Ptr (Handle Poly.Triangulation))
+foreign import capi unsafe "hs_BRep_Tool.h hs_BRep_Tool_triangulation" rawTriangulation
+    :: Ptr (TopoDS.Face)
+    -> Ptr TopLoc.Location
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr (Handle Poly.Triangulation))
 
 triangulation :: Ptr TopoDS.Face -> Ptr TopLoc.Location -> Acquire (Ptr (Handle Poly.Triangulation))
-triangulation face loc = mkAcquire (rawTriangulation face loc) deleteHandleTriangulation
+triangulation face loc = mkAcquire (wrapException $ rawTriangulation face loc) deleteHandleTriangulation
