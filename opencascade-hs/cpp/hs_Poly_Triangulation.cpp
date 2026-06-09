a632c772e6e07f11487d5eefe0957aab5da6603c
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-09 16:36:10 +0100

    migrate Poly.Triangulation to new exception handling

--- opencascade-hs/cpp/hs_Poly_Triangulation.cpp
+++ opencascade-hs/cpp/hs_Poly_Triangulation.cpp
@@ -1,8 +1,18 @@
 #include <Poly_Triangulation.hxx>
+#include "hs_Exception.h"
 #include "hs_Poly_Triangulation.h"
 
-Handle(Poly_Triangulation) * hs_new_Poly_Triangulation(int nbNodes, int nbTriangles, bool hasUVNodes, bool hasNormals){
-    return new opencascade::handle<Poly_Triangulation>(new Poly_Triangulation(nbNodes, nbTriangles, hasUVNodes, hasNormals));
+Handle(Poly_Triangulation) * hs_new_Poly_Triangulation(
+        int nbNodes, int nbTriangles, bool hasUVNodes, bool hasNormals,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [nbNodes, nbTriangles, hasUVNodes, hasNormals]{
+            return new opencascade::handle<Poly_Triangulation>(new Poly_Triangulation(nbNodes, nbTriangles, hasUVNodes, hasNormals));
+        }
+    );
 }
 
 void hs_delete_Poly_Triangulation(Handle(Poly_Triangulation) * triangulation){
@@ -18,18 +28,54 @@ int hs_Poly_Triangulation_nbTriangles(Handle(Poly_Triangulation) * triangulation
 }
 
 
-gp_Pnt * hs_Poly_Triangulation_node(Handle(Poly_Triangulation) * triangulation, int index){
-    return new gp_Pnt(triangulation->get()->Node(index));
+gp_Pnt * hs_Poly_Triangulation_node(
+        Handle(Poly_Triangulation) * triangulation, int index,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [triangulation, index]{
+            return new gp_Pnt(triangulation->get()->Node(index));
+        }
+    );
 }
 
-void hs_Poly_Triangulation_setNode(Handle(Poly_Triangulation) * triangulation, int index, gp_Pnt * pnt){
-    triangulation->get()->SetNode(index, *pnt);
+void hs_Poly_Triangulation_setNode(
+        Handle(Poly_Triangulation) * triangulation, int index, gp_Pnt * pnt,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(
+        exType,
+        exPtr,
+        [triangulation, index, pnt]{
+            triangulation->get()->SetNode(index, *pnt);
+        }
+    );
 }
 
-Poly_Triangle * hs_Poly_Triangulation_triangle(Handle(Poly_Triangulation) * triangulation, int index){
-    return new Poly_Triangle(triangulation->get()->Triangle(index));
+Poly_Triangle * hs_Poly_Triangulation_triangle(
+        Handle(Poly_Triangulation) * triangulation, int index,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [triangulation, index]{
+            return new Poly_Triangle(triangulation->get()->Triangle(index));
+        }
+    );
 }
 
-void hs_Poly_Triangulation_setTriangle(Handle(Poly_Triangulation) * triangulation, int index, Poly_Triangle * triangle){
-    triangulation->get()->SetTriangle(index, *triangle);
+void hs_Poly_Triangulation_setTriangle(
+        Handle(Poly_Triangulation) * triangulation, int index, Poly_Triangle * triangle,
+        HSExceptionType* exType, void ** exPtr
+){
+    hs_handleExVoid(
+        exType,
+        exPtr,
+        [triangulation, index, triangle]{
+            triangulation->get()->SetTriangle(index, *triangle);
+        }
+    );
 }
--- opencascade-hs/src/OpenCascade/Poly/Triangulation.hs
+++ opencascade-hs/src/OpenCascade/Poly/Triangulation.hs
@@ -18,12 +18,20 @@ import OpenCascade.Handle (Handle)
 import Foreign.Ptr (Ptr)
 import Foreign.C (CInt (..), CBool (..))
 import OpenCascade.Internal.Bool (boolToCBool)
+import OpenCascade.Internal.Exception (wrapException)
 import Data.Acquire (Acquire, mkAcquire)
 
-foreign import capi unsafe "hs_Poly_Triangulation.h hs_new_Poly_Triangulation" rawNew :: CInt -> CInt -> CBool -> CBool -> IO (Ptr (Handle Triangulation))
+foreign import capi unsafe "hs_Poly_Triangulation.h hs_new_Poly_Triangulation" rawNew
+    :: CInt
+    -> CInt
+    -> CBool
+    -> CBool
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr (Handle Triangulation))
 
 new :: Int -> Int -> Bool -> Bool -> Acquire (Ptr (Handle Triangulation))
-new nNodes nTriangles hasUVNodes hasNormals = mkAcquire (rawNew (fromIntegral nNodes) (fromIntegral nTriangles) (boolToCBool hasUVNodes) (boolToCBool hasNormals)) deleteHandleTriangulation
+new nNodes nTriangles hasUVNodes hasNormals = mkAcquire (wrapException $ rawNew (fromIntegral nNodes) (fromIntegral nTriangles) (boolToCBool hasUVNodes) (boolToCBool hasNormals)) deleteHandleTriangulation
 
 foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_nbNodes" rawNbNodes :: Ptr (Handle Triangulation) -> IO CInt
 
@@ -35,22 +43,44 @@ foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_nbTria
 nbTriangles :: Ptr (Handle Triangulation) -> IO Int
 nbTriangles tri = fromIntegral <$> rawNbTriangles tri
 
-foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_node" rawNode :: Ptr (Handle Triangulation) -> CInt -> IO (Ptr GP.Pnt)
+foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_node" rawNode
+    :: Ptr (Handle Triangulation)
+    -> CInt
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr GP.Pnt)
 
 node :: Ptr (Handle Triangulation) -> Int -> Acquire (Ptr GP.Pnt)
-node tri index = mkAcquire (rawNode tri (fromIntegral index)) deletePnt
+node tri index = mkAcquire (wrapException $ rawNode tri (fromIntegral index)) deletePnt
 
-foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_setNode" rawSetNode :: Ptr (Handle Triangulation) -> CInt -> Ptr GP.Pnt -> IO()
+foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_setNode" rawSetNode
+    :: Ptr (Handle Triangulation)
+    -> CInt
+    -> Ptr GP.Pnt
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO ()
 
 setNode :: Ptr (Handle Triangulation) -> Int -> Ptr GP.Pnt -> IO ()
-setNode tri index pnt = rawSetNode tri (fromIntegral index) pnt
+setNode tri index pnt = wrapException $ rawSetNode tri (fromIntegral index) pnt
 
-foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_triangle" rawTriangle :: Ptr (Handle Triangulation) -> CInt -> IO (Ptr Triangle)
+foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_triangle" rawTriangle
+    :: Ptr (Handle Triangulation)
+    -> CInt
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr Triangle)
 
 triangle :: Ptr (Handle Triangulation) -> Int -> Acquire (Ptr Triangle)
-triangle tri index = mkAcquire (rawTriangle tri (fromIntegral index)) deleteTriangle
+triangle tri index = mkAcquire (wrapException $ rawTriangle tri (fromIntegral index)) deleteTriangle
 
-foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_setTriangle" rawSetTriangle :: Ptr (Handle Triangulation) -> CInt -> Ptr Triangle -> IO ()
+foreign import capi unsafe "hs_Poly_Triangulation.h hs_Poly_Triangulation_setTriangle" rawSetTriangle
+    :: Ptr (Handle Triangulation)
+    -> CInt
+    -> Ptr Triangle
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO ()
 
 setTriangle :: Ptr (Handle Triangulation) -> Int -> Ptr Triangle -> IO ()
-setTriangle tri index theTriangle = rawSetTriangle tri (fromIntegral index) theTriangle 
+setTriangle tri index theTriangle = wrapException $ rawSetTriangle tri (fromIntegral index) theTriangle
