21ec6a53709442d07989910e8c27fdb8f1dc238c
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-09 15:53:00 +0100

    Migrate GeomAdaptor onto new exception handling

--- opencascade-hs/src/OpenCascade/GeomAdaptor/Curve.hs
+++ opencascade-hs/src/OpenCascade/GeomAdaptor/Curve.hs
@@ -12,21 +12,30 @@ import qualified OpenCascade.Geom.Types as Geom
 import qualified OpenCascade.Geom.Internal.Destructors as Geom.Destructors
 import OpenCascade.GeomAdaptor.Internal.Destructors (deleteCurve)
 import OpenCascade.Handle (Handle)
-import Foreign.C (CDouble (..))
+import OpenCascade.Internal.Exception (wrapException)
+import Foreign.C (CDouble (..), CInt)
 import Data.Coerce (coerce)
-import Foreign.Ptr 
+import Foreign.Ptr
 import Data.Acquire (Acquire, mkAcquire)
 
-foreign import capi unsafe "hs_GeomAdaptor_Curve.h hs_new_GeomAdaptor_Curve_fromHandle" rawFromHandle :: Ptr (Handle Geom.Curve) -> IO (Ptr Curve)
+foreign import capi unsafe "hs_GeomAdaptor_Curve.h hs_new_GeomAdaptor_Curve_fromHandle" rawFromHandle
+    :: Ptr (Handle Geom.Curve)
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr Curve)
 
 fromHandle :: Ptr (Handle Geom.Curve) -> Acquire (Ptr Curve)
-fromHandle h = mkAcquire (rawFromHandle h) deleteCurve
+fromHandle h = mkAcquire (wrapException $ rawFromHandle h) deleteCurve
 
 
-foreign import capi unsafe "hs_GeomAdaptor_Curve.h hs_GeomAdaptor_Curve_curve" rawCurve :: Ptr Curve -> IO (Ptr (Handle Geom.Curve))
+foreign import capi unsafe "hs_GeomAdaptor_Curve.h hs_GeomAdaptor_Curve_curve" rawCurve
+    :: Ptr Curve
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr (Handle Geom.Curve))
 
 curve :: Ptr Curve -> Acquire (Ptr (Handle Geom.Curve))
-curve adaptor = mkAcquire (rawCurve adaptor) Geom.Destructors.deleteHandleCurve
+curve adaptor = mkAcquire (wrapException $ rawCurve adaptor) Geom.Destructors.deleteHandleCurve
 
 foreign import capi unsafe "hs_GeomAdaptor_Curve.h hs_GeomAdaptor_Curve_firstParameter" rawFirstParameter :: Ptr Curve-> IO (CDouble)
 
