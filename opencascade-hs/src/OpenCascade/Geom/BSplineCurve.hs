5cb0eb9a5dd5b635ed139259238d4d011ba099eb
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-09 16:33:03 +0100

    Migrate Geom.BSplineCurve onto new exception handling

--- opencascade-hs/src/OpenCascade/Geom/BSplineCurve.hs
+++ opencascade-hs/src/OpenCascade/Geom/BSplineCurve.hs
@@ -15,6 +15,7 @@ import OpenCascade.Handle (Handle)
 import OpenCascade.Internal.Bool (cBoolToBool)
 import OpenCascade.GP (Pnt)
 import OpenCascade.GP.Internal.Destructors (deletePnt)
+import OpenCascade.Internal.Exception (wrapException)
 import Data.Coerce (coerce)
 
 foreign import capi unsafe "hs_Geom_BSplineCurve.h hs_Geom_BSplineCurve_toHandle" rawToHandle :: Ptr BSplineCurve -> IO (Ptr (Handle BSplineCurve))
@@ -27,18 +28,30 @@ foreign import capi unsafe "hs_Geom_BSplineCurve.h hs_Geom_BSplineCurve_nbPoles"
 nbPoles :: Ptr (Handle (BSplineCurve)) -> IO Int 
 nbPoles h = fromIntegral <$> rawNbPoles h
 
-foreign import capi unsafe "hs_Geom_BSplineCurve.h hs_Geom_BSplineCurve_pole" rawPole :: Ptr (Handle BSplineCurve) -> CInt -> IO (Ptr Pnt)
+foreign import capi unsafe "hs_Geom_BSplineCurve.h hs_Geom_BSplineCurve_pole" rawPole
+    :: Ptr (Handle BSplineCurve)
+    -> CInt
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO (Ptr Pnt)
 
 pole :: Ptr (Handle BSplineCurve) -> Int -> Acquire (Ptr Pnt)
-pole h n = mkAcquire (rawPole h (fromIntegral n)) deletePnt
+pole h n = mkAcquire (wrapException $ rawPole h (fromIntegral n)) deletePnt
 
 foreign import capi unsafe "hs_Geom_BSplineCurve.h hs_Geom_BSplineCurve_isRational" rawIsRational :: Ptr (Handle BSplineCurve) -> IO (CBool)
 
 isRational :: Ptr (Handle (BSplineCurve)) -> IO Bool
 isRational h = cBoolToBool <$> rawIsRational h
 
-foreign import capi unsafe "hs_Geom_BSplineCurve.h hs_Geom_BSplineCurve_segment" rawSegment :: Ptr (Handle BSplineCurve) -> CDouble -> CDouble -> CDouble -> IO ()
+foreign import capi unsafe "hs_Geom_BSplineCurve.h hs_Geom_BSplineCurve_segment" rawSegment
+    :: Ptr (Handle BSplineCurve)
+    -> CDouble
+    -> CDouble
+    -> CDouble
+    -> Ptr CInt
+    -> Ptr (Ptr ())
+    -> IO ()
 
 segment :: Ptr (Handle BSplineCurve) -> Double -> Double -> Double -> IO ()
-segment = coerce rawSegment
+segment h u1 u2 confusion = wrapException $ rawSegment h (coerce u1) (coerce u2) (coerce confusion)
 
