416be239bb90d2d375313f8bcdf41e47e30bd016
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-11 16:07:44 +0100

    Migrate GProp.GProps onto new excetion handling

--- opencascade-hs/cpp/hs_GProp_GProps.cpp
+++ opencascade-hs/cpp/hs_GProp_GProps.cpp
@@ -1,26 +1,51 @@
 #include <GProp_GProps.hxx>
+#include "hs_Exception.h"
 #include "hs_GProp_GProps.h"
 
-GProp_GProps * hs_new_GProp_GProps(){
-    return new GProp_GProps();
+GProp_GProps * hs_new_GProp_GProps(
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(exType, exPtr, []{
+        return new GProp_GProps();
+    });
 }
 
-GProp_GProps * hs_new_GProp_GProps_fromSystemLocation(gp_Pnt * pnt){
-    return new GProp_GProps(*pnt);
+GProp_GProps * hs_new_GProp_GProps_fromSystemLocation(
+        gp_Pnt * pnt,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(exType, exPtr, [pnt]{
+        return new GProp_GProps(*pnt);
+    });
 }
 
 void  hs_delete_GProp_GProps(GProp_GProps * props){
     delete props;
 }
 
-double hs_GProp_GProps_mass(GProp_GProps * props){
-    return props->Mass();
+double hs_GProp_GProps_mass(
+        GProp_GProps * props,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleExWithDefault(exType, exPtr, [props]{
+        return props->Mass();
+    }, 0.0);
 }
 
-gp_Pnt * hs_GProp_GProps_centreOfMass(GProp_GProps * props){
-    return new gp_Pnt(props->CentreOfMass());
+gp_Pnt * hs_GProp_GProps_centreOfMass(
+        GProp_GProps * props,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(exType, exPtr, [props]{
+        return new gp_Pnt(props->CentreOfMass());
+    });
 }
 
-double hs_GProp_GProps_momentOfInertia(GProp_GProps * props, gp_Ax1 * ax){
-    return props->MomentOfInertia(*ax);
+double hs_GProp_GProps_momentOfInertia(
+        GProp_GProps * props, gp_Ax1 * ax,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleExWithDefault(exType, exPtr, [props, ax]{
+        return props->MomentOfInertia(*ax);
+    }, 0.0);
 }
--- opencascade-hs/cpp/hs_GProp_GProps.h
+++ opencascade-hs/cpp/hs_GProp_GProps.h
@@ -7,17 +7,31 @@
 extern "C" {
 #endif
 
-GProp_GProps * hs_new_GProp_GProps();
+GProp_GProps * hs_new_GProp_GProps(
+    HSExceptionType* exType, void ** exPtr
+);
 
-GProp_GProps * hs_new_GProp_GProps_fromSystemLocation(gp_Pnt * pnt);
+GProp_GProps * hs_new_GProp_GProps_fromSystemLocation(
+    gp_Pnt * pnt,
+    HSExceptionType* exType, void ** exPtr
+);
 
 void  hs_delete_GProp_GProps(GProp_GProps * props);
 
-double hs_GProp_GProps_mass(GProp_GProps * props);
+double hs_GProp_GProps_mass(
+    GProp_GProps * props,
+    HSExceptionType* exType, void ** exPtr
+);
 
-gp_Pnt * hs_GProp_GProps_centreOfMass(GProp_GProps * props);
+gp_Pnt * hs_GProp_GProps_centreOfMass(
+    GProp_GProps * props,
+    HSExceptionType* exType, void ** exPtr
+);
 
-double hs_GProp_GProps_momentOfInertia(GProp_GProps * props, gp_Ax1 * ax);
+double hs_GProp_GProps_momentOfInertia(
+    GProp_GProps * props, gp_Ax1 * ax,
+    HSExceptionType* exType, void ** exPtr
+);
 
 #ifdef __cplusplus
 }
