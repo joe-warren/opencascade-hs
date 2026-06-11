acd9ecffb167338c93a4d3481196846871203b67
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-11 10:53:36 +0100

    Migrate Font.BRepFont onto new exception handling

--- opencascade-hs/cpp/hs_Font_BRepFont.cpp
+++ opencascade-hs/cpp/hs_Font_BRepFont.cpp
@@ -1,4 +1,5 @@
 #include <Font_BRepFont.hxx>
+#include "hs_Exception.h"
 #include "hs_Font_BRepFont.h"
 
 
@@ -6,20 +7,49 @@ Font_BRepFont * hs_new_Font_BRepFont(){
     return new Font_BRepFont();
 }
 
-Font_BRepFont * hs_new_Font_BRepFont_fromStringAndSize(char * path, double size){
-    return new Font_BRepFont(path, size);
+Font_BRepFont * hs_new_Font_BRepFont_fromStringAndSize(
+        char * path, double size,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleEx(
+        exType,
+        exPtr,
+        [path, size]{
+            return new Font_BRepFont(path, size);
+        }
+    );
 }
 
 void hs_delete_Font_BRepFont(Font_BRepFont * font){
     delete font;
 }
 
-bool hs_Font_BRepFont_initPathAndSize(Font_BRepFont * font, char * path, double size){
-    return font->Init(path, size, 0);
+bool hs_Font_BRepFont_initPathAndSize(
+        Font_BRepFont * font, char * path, double size,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleExWithDefault(
+        exType,
+        exPtr,
+        [font, path, size]{
+            return font->Init(path, size, 0);
+        },
+        false
+    );
 }
 
-bool hs_Font_BRepFont_initNameAspectAndSize(Font_BRepFont * font, char * name, Font_FontAspect aspect, double size){
-    return font->Init(name, aspect, size);
+bool hs_Font_BRepFont_initNameAspectAndSize(
+        Font_BRepFont * font, char * name, Font_FontAspect aspect, double size,
+        HSExceptionType* exType, void ** exPtr
+){
+    return hs_handleExWithDefault(
+        exType,
+        exPtr,
+        [font, name, aspect, size]{
+            return font->Init(name, aspect, size);
+        },
+        false
+    );
 }
 
 double hs_Font_BRepFont_ascender(Font_BRepFont * font){
