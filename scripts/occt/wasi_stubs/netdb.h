414713eedea9c18bab529302951518c7ebdf0f94
Joseph Warren <hungryjoe.warren@gmail.com>
2026-06-18 19:57:31 +0100

    Switch from patching OCCT to a fork of it

--- scripts/occt/wasi_stubs/netdb.h
+++
@@ -1,6 +0,0 @@
-#ifndef _WASI_STUB_NETDB_H
-#define _WASI_STUB_NETDB_H
-struct hostent { char *h_name; char **h_aliases; int h_addrtype; int h_length; char **h_addr_list; };
-static inline struct hostent* gethostbyname(const char* name) { (void)name; return 0; }
-static inline int gethostname(char* name, int len) { if (len > 0) name[0] = 0; return -1; }
-#endif
